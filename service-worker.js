e22a1cc81b9126ca3b55a20ca937284077f2f9b9
tonykuo2002 <31689110+tonykuo2002@users.noreply.github.com>
2019-12-02 03:11:38 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,43 @@
 // CODELAB: Add list of files to cache here.
+const CACHE_NAME = 'shell-content';
 const FILES_TO_CACHE = [
   '/offline.html',
 ];
 
 // CODELAB: Precache static resources here.
-evt.waitUntil(
+self.addEventListener('install', function(evt) {
+  console.log('[ServiceWorker] Install');
+  evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
-);
+  );
+  
+  // CODELAB: Remove previous cached data from disk.
+  evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
 
-// CODELAB: Remove previous cached data from disk.
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-);
+  // CODELAB: Add fetch event handler here.
 
-// CODELAB: Add fetch event handler here.
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('offline.html');
+                });
+          })
+  );
 
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-              .then((cache) => {
-                return cache.match('offline.html');
-              });
-        })
-);
+  
+});
+
