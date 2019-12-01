b769e78781ed5d3c8e6230c73c45aaed04bf58ef
tonykuo2002 <31689110+tonykuo2002@users.noreply.github.com>
2019-12-02 03:15:56 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,20 +24,23 @@ self.addEventListener('install', function(evt) {
           }
         }));
       })
-  );
+  );  
+});
 
-  // CODELAB: Add fetch event handler here.
-
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
-  );
-
-  
+self.addEventListener('fetch', function(event) {
+    // CODELAB: Add fetch event handler here.
+    if (event.request.mode !== 'navigate') {
+      // Not a page navigation, bail.
+      return;
+    }
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+              return caches.open(CACHE_NAME)
+                  .then((cache) => {
+                    return cache.match('offline.html');
+                  });
+            })
+    );
 });
 
